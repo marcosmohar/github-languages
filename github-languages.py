9c6a8edb789811f83769b57cf1b9ae90ff0ff1c4
Marcos Mohar <elcabeca@gmail.com>
2014-12-12 00:14:08 -0600

    initial commit

---
+++ github-languages.py
@@ -0,0 +1,46 @@
+import sys
+import operator
+from collections import defaultdict
+
+import requests
+from secret import USERNAME, PASSWORD
+
+def get_repositories(user):
+	""" Retrieve a list of a user's repositories"""
+	url = "https://api.github.com/users/{user}/repos".format(user=user)
+	response = requests.get(url, auth=(USERNAME, PASSWORD))
+	return response.json()
+
+def get_language_dictionaries(repositories):
+	""" 
+	Return a list of dictionaries containing
+	the languages used in each repository
+	"""
+	language_dictionaries =[]
+	for repository in repositories:
+		url = "https://api.github.com/repos/{owner}/{repo}/languages"
+		url = url.format(owner=repository['owner']['login'], repo=repository["name"])
+		response = requests.get(url, auth=(USERNAME,PASSWORD))
+		language_dictionaries.append(response.json())
+	return language_dictionaries
+
+def accumulate_languages(language_dictionaries):
+	""" Calculate the total data size for each language """
+	accumulated = defaultdict(int)
+	total = 0
+	for language_dictionary in language_dictionaries:
+		for language_name, number_of_bytes in language_dictionary:
+			accumulated[language_name] += number_of_bytes
+			total += number_of_bytes
+	return accumulated, total
+
+def main():
+	""" Main function """
+	repositories = get_repositories(sys.argv[1])
+	language_dictionaries = get_language_dictionaries(repositories)
+	print language_dictionaries
+
+
+
+if __name__ == "__main__":
+	main()
